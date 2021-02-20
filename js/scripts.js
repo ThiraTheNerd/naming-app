039c0ef522108bd97c3f9c0fd39d71f97d7ee83b
ThiraTheNerd <thiragithinji@gmail.com>
2021-02-20 08:55:48 +0300

    define the variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,4 @@
-<!-- Begin
-function calculate() {
-month = document.form.month.selectedIndex;
-month = document.form.month.options[month].value;
-day = document.form.day.selectedIndex;
-day = document.form.day.options[day].value;
-year = document.form.year.value;
-var dob = month + " " + day + ", " + year;
-var then = new Date(dob);
-var theday = then.getDay()+1;
-var date=then.getDate();
-var year=then.getYear();
+var CC, YY, MM, DD, d, dayValue;
+var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
