8c254d63cfcaf87b458929a3e093424dd8f0358d
ThiraTheNerd <thiragithinji@gmail.com>
2021-02-20 14:42:07 +0300

    add the month verification code

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,16 @@ var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 
 function validate(){
   var genders = document.getElementsByName("gender")
-  if(document.myForm.year.value == "" || document.myForm.year.value.length != 4 || document.myForm.year.value > 2100 || document.myForm.year.value <= 1900){
+  if(document.myForm.year.value == "" || document.myForm.year.value.length != 4 || document.myForm.year.value > 2100
+   || document.myForm.year.value <= 1900){
     alert("Please provide a valid year of birth! eg. 2020");
     document.myForm.year.focus();
     return false;
   }
+  else if (document.myForm.month.value == "" || isNaN( document.myForm.month.value) || document.myForm.month.value.length != 2 || document.myForm.month.value > 12 || document.myForm.value <= 0){
+    alert("Please enter a valid month ! any number between 1 and 12");
+    document.myForm.month.focus();
+    return false;
+  } 
+
 }
